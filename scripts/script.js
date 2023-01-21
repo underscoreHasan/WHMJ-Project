d70125106da56045c2a967158375389ddd4bab85
nlmanama <nlminhanh12@gmail.com>
2023-01-21 13:43:20 -0800

    Declared data type

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function Journey (b1c, b1n, b2c, b2n, day){
+        this.n1 = b1c;
+        this.c1 = b1n;
+        this.n2 = b2c;
+        this.c2 = b2n;
+        this.time = 0;
+        this.day = day;
+}
