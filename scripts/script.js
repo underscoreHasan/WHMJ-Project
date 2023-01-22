623b883a75a43374f13150d95c29257025db7b5c
underscoreHasan <109660327+underscoreHasan@users.noreply.github.com>
2023-01-21 17:11:36 -0800

    Update script.js
    added complete example data

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,24 @@
-function Journey (c1, b1, c2, b2, day, term){
-        this.c1 = c1;
-        this.b1 = b1;
-        this.c2 = c2;
-        this.b2 = b2;
+function Journey (class1, buil1, class2, buil2, day, term){
+        this.class1 = class1;
+        this.buil1 = buil1;
+        this.class2 = class2;
+        this.buil2 = buil2;
         this.day = day;
         this.term = term;
         this.time = 0;
+        this.online = false;
 }
 
-let J1 = new Journey('CPSC', 'West Mall Swing Space', 'CHEM', 'Chemistry', 'MO', 1);
-let J2 = new Journey('DSCI', 'Hennings', '', '', 'TU', 1);
-let J3 = new Journey('CPSC', 'West Mall Swing Space', 'CHEM', 'Chemistry', 'WE', 1);
-let J4 = new Journey('DSCI', 'Hennings', 'CHEM', 'Chemistry', 'TH', 1);
-let J4 = new Journey('CHEM', 'Chemistry', 'CHEM', 'Chemistry', 'TH', 1);
-let J4 = new Journey('CPSC', 'Institute for Comput', 'CHEM', 'Chemistry', 'TH', 1);
+let J1 = new Journey('CPSC 110 103', 'West Mall Swing Space', 'CHEM 111 112', 'Chemistry', 'Mon', 1);
+let J2 = new Journey('DSCI 100 004', 'Hennings', '', '', 'Tue', 1);
+let J3 = new Journey('CPSC 110 103', 'West Mall Swing Space', 'CHEM 111 112', 'Chemistry', 'Wed', 1);
+let J4 = new Journey('DSCI 100 004', 'Hennings', 'CHEM 111 112', 'Chemistry', 'Thu', 1);
+let J5 = new Journey('CHEM 111 112', 'Chemistry', 'CHEM 111 L07', 'Chemistry', 'Thu', 1);
+let J6 = new Journey('CPSC 110 L19', 'Institute for Computing (ICICS/CS)', 'CHEM 111 112', 'Chemistry', 'Fri', 1);
 
-
-
-console.log(J1.time);
-console.log(J1.day)
-
-/*!!! Some sections do not have an assigned room*/
-
-/*var journeyList = [];*/
-
-/*UBC ics file structure
-- Every period is a seperate VEVENT
-- One single period can be spotted by it's beginning 'BEGIN:VEVENT' and ending 'END:VEVENT' tag
-- Periods are ordered by term, and then by alphabetical order of their 4 letter code*/
+let J7 = new Journey('CPSC 110 103', 'West Mall Swing Space', 'CHEM 111 112', 'Chemistry', 'Mon', 2);
+let J8 = new Journey('DSCI 100 004', 'Hennings', '', '', 'Tue', 2);
+let J9 = new Journey('CPSC 110 103', 'West Mall Swing Space', 'CHEM 111 112', 'Chemistry', 'Wed', 2);
+let J10 = new Journey('DSCI 100 004', 'Hennings', 'CHEM 111 112', 'Chemistry', 'Thu', 2);
+let J11 = new Journey('CHEM 111 112', 'Chemistry', 'CHEM 111 L07', 'Chemistry', 'Thu', 2);
+let J12 = new Journey('CPSC 110 L19', 'Institute for Computing (ICICS/CS)', 'CHEM 111 112', 'Chemistry', 'Fri', 2);
